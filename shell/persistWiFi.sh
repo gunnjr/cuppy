72ff685b53429acfe08f49f9a2af880bbfb2deb9
John Gunn <john@jrgunn.com>
2020-10-12 07:48:40 -0400

    Updated persistWiFi script to loop and log

--- shell/persistWiFi.sh
+++ shell/persistWiFi.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-if ! [ "$(ping -c 1 192.168.20.1)" ]; then
-    sudo ifdown wlan0
-    sudo ifup wlan0
+attempts=0
+maxAttempts=5
+
+
+while [[ ( $attempts -lt $maxAttempts ) && ( ! "$(ping -c 1 192.168.20.1)" ) ]]
+do
+    attempts=$((attempts+1))
+    echo `date` ": WiFi Down. Attempting to Reconnect ($attempts of $maxAttempts)..."
+    # sudo ifdown wlan0
+    sleep 2
+    # sudo ifup wlan0
+done
+
+if  [ ! "$(ping -c 1 192.168.20.1)" ]; then
+    echo "Failed to reconnect to WiFi after $attempts attempts."
 fi
